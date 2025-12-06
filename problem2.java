b8a433c074932c15263f632db64751f68e70beb2
Yogita <yogitapandit2505@gmail.com>
2025-12-06 20:43:46 +0530

    problem2.java

--- problem2.java
+++ problem2.java
@@ -1,45 +1,32 @@
 public class problem2 {
 
     public static void main(String[] args) {
-
-        // ▼▼▼  PUT YOUR INPUT ARRAY HERE  ▼▼▼
         int[] nums = {1, 2, 3, 4};
-        // ▲▲▲  Change values for testing  ▲▲▲
-
         Solution sol = new Solution();
         int result = sol.countPartitions(nums);
-
         System.out.println("Valid partitions = " + result);
     }
 }
-
 class Solution {
-
-    /**
-     * Counts number of valid prefix partitions
-     * where |leftSum - rightSum| is even.
-     */
     public int countPartitions(int[] nums) {
-        int leftSum = 0;
-        int rightSum = 0;
+        int n = nums.length;
 
-        // calculate total sum
-        for (int num : nums) {
-            rightSum += num;
+        int sum = 0;
+        for (int i=0; i<=n-2; i++){
+            sum+= nums[i];
         }
-
-        int partitionCount = 0;
-
-        // split positions from 0 to n-2
-        for (int i = 0; i < nums.length - 1; i++) {
+        int result = 0;
+        int leftSum = 0;
+        for (int i = 0; i <= n - 2; i++) {
             leftSum += nums[i];
-            rightSum -= nums[i];
+
+            int rightSum = sum - leftSum;
 
             if ((leftSum - rightSum) % 2 == 0) {
-                partitionCount++;
+                result++;
             }
         }
 
-        return partitionCount;
+        return result;
     }
 }
